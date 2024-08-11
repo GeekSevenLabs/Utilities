842be08a096d8710f958cfa1fc9b8b682f0ee724
Emerson Trindade (MensoDev) <emerson.trindade@codeblox.com.br>
2024-08-10 21:46:13 -0300

    ⭐ add support for BrazilianIndividualRegistration (CPF)

---
+++ src/Documents/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace GeekSevenLabs.Utilities.Documents;
+
+internal static class StringExtensions
+{
+    public static string RemoveNonNumericCharacters(this string value)
+    {
+        return string.IsNullOrWhiteSpace(value) ? string.Empty : new string(value.Where(char.IsDigit).ToArray());
+    }
+}
