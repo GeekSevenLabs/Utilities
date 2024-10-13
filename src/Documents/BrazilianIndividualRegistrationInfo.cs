c92b5ec0cc1469791d6a5b572f1f978cf084017f
Emerson Trindade (MensoDev) <emerson.trindade@codeblox.com.br>
2024-10-13 19:14:23 -0300

    🪛 fix names and tests

---
+++ src/Documents/BrazilianIndividualRegistrationInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace GeekSevenLabs.Utilities.Documents;
+
+public readonly struct BrazilianIndividualRegistrationInfo
+{
+    public required string Value { get; init; }
+    public required bool IsValid { get; init; }
+
+    public required int FirstVerificationDigit { get; init; }
+    public required int SecondVerificationDigit { get; init; }
+    public required int Region { get; init; }
+}
